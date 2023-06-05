97bde3948b8218f47b48c43404fc0750260f61ba
oz <ozshimon21@gmail.com>
2023-06-05 11:36:04 -0700

    add api routes prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { GameHistoryDto } from './chess/dtos/game-history.dto';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1');
+
   const config = new DocumentBuilder()
     .setTitle('Chess as as service')
     .setDescription('Chess Game Nest.js Backend API')
